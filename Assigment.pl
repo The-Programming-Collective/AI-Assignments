df1c861375fbad826f8b2092f725dc7f9e245229
MainUseless <86481662+MainUseless@users.noreply.github.com>
2023-04-22 21:07:12 +0200

    fixes

--- Assigment.pl
+++ Assigment.pl
@@ -1,20 +1,25 @@
-%Creates an empty space state with propper size
-createSpace([X|T],Locations,Space) :-
-    T = [Y|_],
+% X = bomb
+% D = domino
+
+% up is loc-y
+% down is loc+y
+% right is loc+1
+% left is loc-1
+
+%Creates initial state space with bombs
+createSpace([X|[Y|_]],Locations,Space) :-
     %length of the list
     Len is X*Y,
     length(TempSpace, Len),
-    placeBombs(Locations,Y,TempSpace,Space).
+    placeBombs(Locations,Y,TempSpace,Space),!.
 
 
 placeBombs([],_,Space,Space).
-placeBombs([H|T],YCoord,TempSpace,Space):-
-    H = [X|T1],
-    T1 = [Y|_],
+placeBombs([ [X|[Y|_] ] |T],YCoord,TempSpace,Space):-
     %location of the bomb
     Loc is ((X-1)*YCoord)+Y,
     placeBomb(Loc,TempSpace,TempTempSpace),
-    placeBombs(T,YpcoCoord,TempTempSpace,Space).
+    placeBombs(T,YCoord,TempTempSpace,Space).
 
 
 %places the bomb in the correct index
@@ -24,5 +29,5 @@ placeBomb(Loc,[H|T],[H|OutSpace]):-
     placeBomb(Nloc,T,OutSpace).
 
 
-%Creates initial state space with bombs
-createSpace(Size,BLocations,Space).
+
+%createSpace(Size,BLocations,Space).
